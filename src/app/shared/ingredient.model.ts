d6cfa12200e82c626ffb02405e3b6c8c57f3a835
Takács Dóra <takacs.dora@effectivegroup.hu>
2018-06-22 12:54:30 +0200

    S03-L53: Shopping-List component content and Ingredient model added

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,13 @@
+export class Ingredient {
+  constructor(public name: string, public amount: number){}
+}
+
+/* This is equal to this:
+  public name: string;
+  public amount: number;
+
+  constructor(name: string, amount: number) {
+    this.name = name;
+    this.amount = amount;
+  }
+ */
