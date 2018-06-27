b776448918e310d53e320f5e90c6a3c5d9a7b11f
Takács Dóra <takacs.dora@effectivegroup.hu>
2018-06-27 12:55:40 +0200

    S10-L108: Using RecipeService for cross-component communication instead of event-chaining.

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -11,10 +11,17 @@ import {RecipeService} from './recipe.service';
 export class RecipesComponent implements OnInit {
   selectedRecipe: Recipe;
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipeService.recipeSelected.subscribe(
+      (recipe: Recipe) => {
+        this.selectedRecipe = recipe;
+      }
+    );
   }
+
+
 
   /*updateRecipeToDetail(recipeToDetail) {
     this.selectedRecipe = recipeToDetail;
