48999e162a96a85978c1fadcd6249a01a24d372b
Takács Dóra <takacs.dora@effectivegroup.hu>
2018-06-28 16:33:57 +0200

    S12-L154: Routes to RecipeEdit added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,22 @@ import {RecipesComponent} from './recipes/recipes.component';
 import {ShoppingListComponent} from './shopping-list/shopping-list.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+
+/*
+* { path: ':id', component: RecipeDetailComponent},
+  { path: 'new', component: RecipeEditComponent},
+  the 'new' url wont work, because it will try to load a Recipe with id='new' instead of id=1 (or other number)
+* */
+
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full'},
   { path: 'recipes', component: RecipesComponent, children: [
       { path: '', component: RecipeStartComponent },
+      { path: 'new', component: RecipeEditComponent}, // comes before the route before dynamic parameter: works correctly
       { path: ':id', component: RecipeDetailComponent},
+      { path: ':id/edit', component: RecipeEditComponent},
     ] },
   { path: 'shopping-list', component: ShoppingListComponent },
 ];
